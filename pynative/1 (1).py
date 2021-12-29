b92931b400a28ea7e9c14a3ed86be8fdd3235fda
blue1335 <d.akkas35@gmail.com>
2021-12-29 17:53:08 +0300

    challenges continue

---
+++ pynative/1 (1).py
@@ -0,0 +1,11 @@
+#basics 
+# Write a program to iterate the first 10 numbers and in each iteration, 
+# print the sum of the current and previous number.
+
+
+for i in range(10):
+    print(i,i+1,"sum:",i+i+1)
+   
+    
+        
+

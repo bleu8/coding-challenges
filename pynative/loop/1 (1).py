f7a62c0a0053fcba6cb0e55c726d2655b6ec2d33
deniz-13 <d.akkas35@gmail.com>
2021-12-31 20:24:53 +0300

    devam

---
+++ pynative/loop/1 (1).py
@@ -0,0 +1,11 @@
+#Exercise 3: Calculate the sum of all numbers from 1 to a given number
+
+
+topla=0
+i=1
+while i<=10:
+    topla+=i
+    i+=1
+print(topla)
+    
+    
---
+++ pynative/loop/1 (4).py
@@ -0,0 +1,20 @@
+"""
+Write a program to display only those numbers from a list that satisfy the following conditions
+
+The number must be divisible by five
+If the number is greater than 150, then skip it and move to the next number
+If the number is greater than 500, then stop the loop
+
+
+"""
+
+numbers = [12, 75, 150, 180, 145, 525, 50]
+# iterate each item of a list
+for item in numbers:
+    if item > 500:
+        break
+    elif item > 150:
+        continue
+    # check if number is divisible by 5
+    elif item % 5 == 0:
+        print(item)
